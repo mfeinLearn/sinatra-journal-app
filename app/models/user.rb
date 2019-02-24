e5bc41a465dcb25b14d87582cc1ace09d6cd7ece
github username <github email address>
2019-02-23 19:47:35 -0500

    add user model file and migration to build user table

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  # bcrypt gives us this method - this allows us to user
+  # a activerecord method called authenticate.
+  # authenticate takes in a password which is a plan string
+  # and checks it agenst bcrypts hashing algurithum to make sure it is
+  # the correct password
+end
