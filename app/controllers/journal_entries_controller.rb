0015ba50247b06299e1b1d53a1e14b44ac6cacbc
github username <github email address>
2019-02-25 19:14:14 -0500

    add post entries, add new form

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -8,7 +8,21 @@ class JournalEntriesController < ApplicationController
 
   # post journal_entries to create a new journal entry
   post '/journal_entries' do
-    
+     #raise params.inspect
+    # I want to create a new journal entry and save it to the DB
+    # I only want to create a journal entry if a user logged in
+    if !logged_in?
+      redirect '/'
+    end
+    # I also only want to save the entry if it has some content
+    if params[:content] != ""
+      # create a new entry
+      @journal_entry = JournalEntry.create(content: params[:content],
+      user_id: current_user.id)
+      redirect "/journal_entries/#{@journal_entry.id}"
+    else
+      redirect '/journal_entries/new'
+    end
   end
   # show route for a journal entry
 
