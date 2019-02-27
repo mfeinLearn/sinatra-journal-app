75eae53e63fdc0c47649fd7786771c867ddb1cd9
github username <github email address>
2019-02-27 14:13:52 -0500

    add flash messages and helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
 
   # the purpose of this route is to render the login page (form)
   get '/login' do
-    puts "**********************FLASH IS "
-    puts flash
+    # puts "**********************FLASH IS "
+    # puts flash
     erb :login
   end
 
@@ -26,9 +26,10 @@ class UsersController < ApplicationController
     session[:user_id] = @user.id # actually logging the user in
     # redirect to the user's show page
     puts session
+    flash[:message] = "Welcome, #{@user.name}!"
     redirect "users/#{@user.id}"
     else
-      flash[:message] = "Your credentials were invalid. Please sign up or try again."
+      flash[:errors] = "Your credentials were invalid. Please sign up or try again."
       # tell the user they entered invalid credentials
       # redirect them to the login page
       redirect '/login'
@@ -61,9 +62,9 @@ class UsersController < ApplicationController
     # }
     # I only want to persist a user that has a name, email, AND
     # password
-    if params[:name] != "" && params[:email] != "" && params[:password] != ""
+    @user = User.create(params)
+    if @user.save
       # valid input
-      @user = User.create(params)
       session[:user_id] = @user.id # actually logging the user in
       # where do I go now?
       # let's go to the user show page
@@ -81,11 +82,14 @@ class UsersController < ApplicationController
 
       # key note: rarely  are we going to render from a post, patch, and delete request
       #  rendering should only happen from a get request
+      flash[:message] = "You have successfully
+      created an accoutn, #{user.name}! Welcome!"
       redirect "/users/#{@user.id}" # the result of this is a brand new get request/ a new http request
     else
       # not valid input
       # it would be better to include a message to the user
       # telling them what is wrong
+      flash[:errors] = "Account creation failure: #{@user.errors.full_messages.to_sentence}"
       redirect '/signup'
     end
 
@@ -100,7 +104,8 @@ class UsersController < ApplicationController
     # what do I need to do first?
     # raise params.inspect
     @user = User.find_by(id: params[:id])
-    #binding.pry
+    
+    redirect_if_not_logged_in
     erb :'users/show'
   end
 
