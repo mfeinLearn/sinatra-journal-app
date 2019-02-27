0c90f7ec0009f57439676e0778b611bfae72380b
github username <github email address>
2019-02-27 12:34:19 -0500

    add AR validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,19 @@ class User < ActiveRecord::Base
   # the correct password
 
   # in forms and our controller we can say password when we
-  # want to refer to our password 
+  # want to refer to our password
+
+    # ACTIVERECORD VALIDATIONS - adding more validations into our user model:
+
+  # validates is a method invocation
+  # name is the thing that is beign validated
+  # presence: true - key value pair
+#  these validations will pervent ActiveRecord from:
+#  - creation saving updating from the database if these requirements are not meet 
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
 
   has_many :journal_entries
 end
